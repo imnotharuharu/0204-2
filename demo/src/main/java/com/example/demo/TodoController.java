111712c962172bcfaeaedc8911a9e8669f6516ae
LAPTOP-CDNSEJAE\academia <202510_aw_tamura@x-ide.sakura.ne.jp>
2026-02-05 11:21:15 +0900

    fixed

--- demo/src/main/java/com/example/demo/TodoController.java
+++ demo/src/main/java/com/example/demo/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.validation.BindingResult;
 import org.springframework.dao.OptimisticLockingFailureException;
