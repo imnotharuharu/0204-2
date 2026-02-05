e2495a62615b39b9ee98943654d87c4b9faab409
LAPTOP-CDNSEJAE\academia <202510_aw_tamura@x-ide.sakura.ne.jp>
2026-02-05 14:57:27 +0900

    ログイン認証

---
+++ demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.AppUser;
+
+public interface UserRepository extends JpaRepository<AppUser, Long> {
+    Optional<AppUser> findByUsername(String username);
+}
